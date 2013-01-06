30242f549b6f82b23194b83316a90196f210d9f1
siddtewari <sidtewari@gmail.com>
2013-01-06 17:18:50 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,82 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
 
-# Name
+# Name: Sidd Tewari
+
+# Chapter 2: Problems 2.1, 2.2, 2.3, and 2.4. 
+# If the question asks for a response, instead of writing code, just write your text in a comment (use # to begin a comment) - 
+# WHERE POSSIBLE, I changed the answer comments to print statements
+
+#------------------------------------------------------------------------------------------------------------------------------------
+# 2.1 - Zip Code - Can you figure out what is going on? Hint: display the values 01, 010, 0100 and 01000.
+print
+print 'EXERCISE 2.1:'
+print 'When typing an integer with a leading value of zero, the interpreter seems to be interpreting it with different bases like Base 2 for 01, Base 3 for 010'
+print
+#------------------------------------------------------------------------------------------------------------------------------------
+# 2.2 - ... Now put the same statements into a script and run it. What is the output? 
+# Modify the script by transforming each expression into a print statement and then run it again.
+
+print 'EXERCISE 2.2:'
+print 5
+x = 5
+print 'x+1 = ', (x + 1), '   ', '(where x = 5)'
+print
+#------------------------------------------------------------------------------------------------------------------------------------
+# 2.3  width = 17
+# height = 12.0
+#delimiter = '.'
+
+#Answers
+print 'EXERCISE 2.3:'
+print '1. width/2 = 8 (floor division) in Python 2; should be 8.5 in Python 3'
+print '2. width/2.0 = 8.5'
+print '3. height/3 = 4.0'
+print '4. 1 + 2 * 5 = 11'
+print "5. delimiter * 5 = '.....'"
+print
+#------------------------------------------------------------------------------------------------------------------------------------
+
+# 2.4    
+    
+# 1. Area of the sphere
+pi = 3.1415926535897932
+area = ((4.0/3.0)*(pi)*(5**3)) #using float 4.0/3.0 instead of integer to accomodate for the python interpreter's floor division and to get the accurate value of 4/3
+print 'EXERCISE 2.4:'
+print 'Area of the sphere is: ', area
+print
+
+# 2. Suppose the cover price of a book is $24.95, but bookstores get a 40% discount. 
+#Shipping costs $3 for the first copy and 75 cents for each additional copy. What is the total wholesale cost for 60 copies?
+cost = 24.95
+discount = 40.0
+discountedCost = cost - ((discount/100)*cost)      #Additional test step - print discountedCost
+
+oneShipCost = 3.0
+addShipCost = 0.75
+
+n = 60.0   #Where n is the number of units to be sold
+
+totalShipCost = (oneShipCost + (addShipCost*(n-1.0)))
+wholesaleCost = ((discountedCost*n) + (totalShipCost))
+print 'EXERCISE 2.4:'
+print 'The wholesaleCost for 60 books is:', wholesaleCost
+print 
+
+# 3. If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at tempo (7:12 per mile) 
+# and 1 mile at easy pace again, what time do I get home for breakfast?
+
+startTime = ((6*60)+52) * 60 # starting time, 06:52:00 am, in seconds
+
+easyPace = ((8*60)+15)  #easy pace of 00:08:15 per mile - in seconds
+tempoPace = ((7*60)+12) #tempo pace of 00:07:12 per mile - in seconds
+
+runTime = (easyPace*1) + (tempoPace*3) + (easyPace*1) #total seconds taken for the five miles run at two different paces
+
+endTime = startTime + runTime #endTime in seconds
+
+endHr = endTime/(60*60) 
+endMin = (endTime%(60*60)) / 60
+
+print 'EXERCISE 3:'
+print ('The runner returns for breakfast at ' + str(endHr) + ':' + str(endMin))
+print
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,185 @@
 # Exercises for chapter 3: Problems 3.1, 3.2, 3.3, and 3.4 in Think Python
 
-# Name
+# Name: Sidd Tewari
+
+# Exercise 3.1. Move the last line of this program to the top, 
+# so the function call appears before the definitions. 
+# Run the program and see what error message you get.
+
+#SOLUTION
+
+# moving repeat_lyrics() - the function call above the function definition leads to an error message
+
+# error message:
+# Traceback (most recent call last):
+#   File "chapThree.py", line 5, in <module>
+#     repeat_lyrics()
+# NameError: name 'repeat_lyrics' is not defined
+
+# repeat_lyrics() - commented out, so that the rest of the program can be executed
+
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay."
+    print "I sleep all night and I work all day."
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+print
+print 'EXERCISE 3.1:'
+print 'TESTED and COMMENTED OUT BECAUSE of the ERROR MESSAGE'
+print "ERROR MESSAGE received - 'repeat_lyrics' is not defined WHEN FUNCTION CALL MOVED ABOVE FUNCTION DEFINTION"
+print
+#-------------------------------------------------------------------------------
+
+# Exercise 3.2. Move the function call back to the bottom and 
+# move the definition of print_lyrics after the definition of 
+# repeat_lyrics. What happens when you run this program?
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+def print_lyrics():
+	print "I'm a lumberjack, and I'm okay."
+	print "I sleep all night and I work all day."
+
+print 'EXERCISE 3.2, LYRICS:'
+repeat_lyrics()
+print
+
+#--------------------------------------------------------------------------------
+
+# Exercise 3.3. Python provides a built-in function called len that returns the length of a string, so
+# the value of len('allen') is 5.
+# Write a function named right_justify that takes a string named s as a parameter and prints the
+# string with enough leading spaces so that the last letter of the string is in column 70 of the display.
+# >>> right_justify('allen')
+
+def right_justify(s):
+	a = len(s)
+	b = 70-a
+	print " "*b, s
+	
+print 'EXERCISE 3.3, RIGHT ALIGNMENT:'	
+right_justify('allen')
+right_justify('benjamin walker')
+right_justify('rehman romano aligator wash')
+right_justify('hi')
+print
+
+#---------------------------------------------------------------------------------------
+
+# Exercise 3.4. A function object is a value you can assign to a variable or pass as an argument. 
+# For example, do_twice is a function that takes a function object as an argument and calls it twice:
+# def do_twice(f):
+#     f()
+#     f()
+# Here's an example that uses do_twice to call a function named print_spam twice.
+# def print_spam():
+#     print 'spam'
+
+# do_twice(print_spam)
+
+#1. Type this example into a script and test it. - TESTED
+
+def do_twice(f):
+	f()
+	f()
+	
+def print_spam():
+	print 'spam'
+
+print 'EXERCISE 3.4, PROBLEM 1 - TESTED:'	
+do_twice(print_spam)
+print
+
+# 2. Modify do_twice so that it takes two arguments, a function object and a value, and calls the function twice, 
+# passing the value as an argument.
+# 3. Write a more general version of print_spam, called print_twice, that takes a string as a parameter and prints it twice.
+# 4. Use the modified version of do_twice to call print_twice twice, passing 'spam' as an argument
+
+def do_twice(f,v):
+	f(v)
+	f(v)
+	
+def print_twice(y):
+	print y
+
+print 'EXERCISE 3.4, PROBLEM 2,3, and 4:'	
+do_twice(print_twice, 'spam')
+print
+
+#5. Define a new function called do_four that takes a function object and a value and calls the function four times, 
+#passing the value as a parameter. There should be only two statements in the body of this function, not four.
+
+def do_deux(f,v):
+	f(v)
+	f(v)
+	
+def print_deux(y):
+	print y
+	
+def do_four(func,val):
+	do_deux(func,val)
+	do_deux(func,val)
+	
+print 'EXERCISE 3.4, PROBLEM 5:'
+do_four(print_deux, 'Four prints from two statements - function calling function!')
+print
+
+#Exercise 3.5. This exercise can be done using only the statements and other features we have learned so far.
+#1. Write a function that draws a grid like the following:
+def draw_twoRow_grid():
+	draw_oneRow_twoCells_head()
+	draw_oneRow_twoCells_head()
+	draw_horizontal_border()      #close out the last row of cells with a bottom border - To avoid OBOE (fencepost problem)
+	
+def draw_oneRow_twoCells_head():
+	draw_horizontal_border()
+	draw_vertical_borders()
+	
+def draw_horizontal_border():
+	print '+', '- ' *4, '+', '- ' *4, '+'
+
+def draw_vertical_borders():
+	print '|', ' ' *8, '|', ' '*8, '|'
+	print '|', ' ' *8, '|', ' '*8, '|'
+	print '|', ' ' *8, '|', ' '*8, '|'
+	print '|', ' ' *8, '|', ' '*8, '|'
+	
+print 'EXERCISE 3.5, PROBLEM 1:'
+print 'Here is the grid with row rows and two columns:'
+draw_twoRow_grid()
+print
+	
+#2. Write a function that draws a similar grid with four rows and four columns.
+
+print
+
+def draw_fourRow_fourCols_grid():
+	draw_oneRow_fourCells_head()
+	draw_oneRow_fourCells_head()
+	draw_oneRow_fourCells_head()
+	draw_oneRow_fourCells_head()
+	draw_hrzl_border()
+	
+def draw_oneRow_fourCells_head():
+		draw_hrzl_border()
+		draw_vert_borders()
+		
+def draw_hrzl_border():
+		print '+', '- ' *4, '+', '- ' *4, '+', '- ' *4, '+', '- ' *4, '+'
+
+def draw_vert_borders():
+		print '|', ' ' *8, '|', ' '*8, '|', ' ' *8, '|', ' '*8, '|'
+		print '|', ' ' *8, '|', ' '*8, '|', ' ' *8, '|', ' '*8, '|'
+		print '|', ' ' *8, '|', ' '*8, '|', ' ' *8, '|', ' '*8, '|'
+		print '|', ' ' *8, '|', ' '*8, '|', ' ' *8, '|', ' '*8, '|'
+
+print 'EXERCISE 3.5, PROBLEM 2:'
+print 'Here is the grid with four rows and four columns:'		
+draw_fourRow_fourCols_grid()
+print 
+	
